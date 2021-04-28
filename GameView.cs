21210295f9a3495e105259dda4a496d415829bbc
Thomas Duthil <tduthil@IUT.BX1>
2021-04-28 09:24:35 +0200

    Initialisation Projet IHM

---
+++ GameView.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseSim2021
+{
+    public partial class GameView : Form
+    {
+        private readonly WorldState theWorld;
+        /// <summary>
+        /// The constructor for the main window
+        /// </summary>
+        public GameView(WorldState world)
+        {
+            InitializeComponent();
+            theWorld = world;
+        }
+        /// <summary>
+        /// Method called by the controler whenever some text should be displayed
+        /// </summary>
+        /// <param name="s"></param>
+        public void WriteLine(string s)
+        {
+            List<string> strs = s.Split('\n').ToList();
+            strs.ForEach(str=>outputListBox.Items.Add(str));
+            if (outputListBox.Items.Count > 0)
+            {
+                outputListBox.SelectedIndex = outputListBox.Items.Count - 1;
+            }
+            outputListBox.Refresh();
+        }
+        /// <summary>
+        /// Method called by the controler whenever a confirmation should be asked
+        /// </summary>
+        /// <returns>Yes iff confirmed</returns>
+        public bool ConfirmDialog()
+        {
+            string message = "Confirmer ?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            return MessageBox.Show(message, "", buttons) == DialogResult.Yes;
+        }
+        #region Event handling
+        private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                e.SuppressKeyPress = true; // Or beep.
+                GameController.Interpret(inputTextBox.Text);
+            }
+        }
+
+        private void GameView_Paint(object sender, PaintEventArgs e)
+        {
+            /*
+            diffLabel.Text = "Difficulté : " + "?";
+            turnLabel.Text = "Tour " + "?";
+            moneyLabel.Text = "Trésor : " + "?" + " pièces d'or";
+            gloryLabel.Text = "Gloire : " + "?";
+            */
+            nextButton.Visible = false;
+        }
+        #endregion
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
